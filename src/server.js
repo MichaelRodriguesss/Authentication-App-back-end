48552f318801c0e5ce5367dcad06c92d43f5ac99
michael <michaeledavi12@gmail.com>
2023-04-14 13:44:18 -0300

    fixed the server port

--- src/server.js
+++ src/server.js
@@ -6,11 +6,13 @@ const app = express();
 
 app.use(express.json());
 
+const port = process.env.PORT || 3000;
+
 dotenv.config();
 database();
 
 app.use("/api/users", routes);
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server started on port 3000 🔥");
 });
