2028046a7238c61909d12bef585ca6e0fb79213e
michael <michaeledavi12@gmail.com>
2023-04-30 12:39:44 -0300

    fixed the multer

--- src/controllers/UpdateController.js
+++ src/controllers/UpdateController.js
@@ -1,3 +1,4 @@
+const multer = require("multer");
 const User = require("../models/User");
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -7,8 +7,9 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3003;
 
 dotenv.config();
 database();
@@ -16,5 +17,5 @@ database();
 app.use("/api/users", routes);
 
 app.listen(port, () => {
-  console.log("Server started on port 3000 🔥");
+  console.log("Server started on port 3003 🔥");
 });
